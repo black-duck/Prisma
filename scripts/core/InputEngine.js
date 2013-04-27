3a629075533a7841a2c3dd88ad7c0a9b82783d7c
George Chatzisofroniou <sophron@latthi.com>
2013-04-27 12:56:26 +0300

    Added the correct versions of core-files.

--- scripts/core/InputEngine.js
+++ scripts/core/InputEngine.js
@@ -17,41 +17,48 @@ InputEngine = {
                 y: 0
         },
 
-
+		portPos: {
+					x:0, 
+					y:0
+		},
 		
         //-----------------------------
         setup: function (canvas) {
 		
 				
-                //move key bindings
-                InputEngine.bind(87, 'look-up');
-                InputEngine.bind(65, 'look-left');
-                InputEngine.bind(68, 'look-right');
+            //move key bindings
+            InputEngine.bind(87, 'look-up');
+            InputEngine.bind(65, 'look-left');
+            InputEngine.bind(68, 'look-right');
 
-                //fire key bindings
-//             InputEngine.bind(81,'powerup-use');  //Q
-//             InputEngine.bind(69,'fire-superWeapon');  //E
-                InputEngine.bind(32,'fire-primary');  //Space
-//             InputEngine.bind(16,'fire-missile');  //Shift   Ascii code=0 key code=16
+            //fire key bindings
+            InputEngine.bind(81,'powerup-use');  //Q
+            InputEngine.bind(69,'fire-superWeapon');  //E
+            InputEngine.bind(32,'fire-primary');  //Space
+            InputEngine.bind(16,'fire-missile');  //Shift   Ascii code=0 key code=16
 
 			
-                //event listeners
-                canvas.addEventListener('mousedown',InputEngine.onMouseDown,true);
-		canvas.addEventListener('mouseout',InputEngine.onMouseUp,true);
-                canvas.addEventListener('mouseup',InputEngine.onMouseUp,true);
-                canvas.addEventListener('mousemove', InputEngine.onMouseMove,true);
-                document.addEventListener('keydown', InputEngine.onKeyDown,true);
-                document.addEventListener('keyup', InputEngine.onKeyUp,true);
+            //event listeners
+        	canvas.addEventListener('mousedown',InputEngine.onMouseDown,true);
+			canvas.addEventListener('mouseout',InputEngine.onMouseUp,true);
+        	canvas.addEventListener('mouseup',InputEngine.onMouseUp,true);
+            canvas.addEventListener('mousemove', InputEngine.onMouseMove,true);
+            document.addEventListener('keydown', InputEngine.onKeyDown,true);
+            document.addEventListener('keyup', InputEngine.onKeyUp,true);
+        	canvas.addEventListener('touchstart',InputEngine.onMouseDown,false);
+        	canvas.addEventListener('touchend',InputEngine.onMouseUp,false);
+			canvas.addEventListener('touchmove',InputEngine.onTouchMove,false);
         },
+
 	onMouseDown:function(event){
  		var action = InputEngine.bindings[32];
 		if (event.button==2){
 			InputEngine.actions[action] = false;
 			return false;
 		}
- 		var rect = document.getElementById('canvas').getBoundingClientRect();
-		InputEngine.mouse.x = (event.clientX-rect.left)*Scale.x;
-        InputEngine.mouse.y = (event.clientY-rect.top)*Scale.y;
+ 		var rect = canvas.getBoundingClientRect();
+			InputEngine.mouse.x = (event.clientX-rect.left+InputEngine.portPos.x)*Scale.x;
+        	InputEngine.mouse.y = (event.clientY-rect.top+InputEngine.portPos.y)*Scale.y;
 		
 		if (action) {
         	InputEngine.actions[action] = true;
@@ -63,37 +70,51 @@ InputEngine = {
                       InputEngine.actions[action] = false;
                 }
 	},
-        //-----------------------------
-        onMouseMove: function (event) {
-        	var rect = document.getElementById('canvas').getBoundingClientRect();
-			InputEngine.mouse.x = (event.clientX-rect.left) * Scale.x;
-            InputEngine.mouse.y = (event.clientY-rect.top) * Scale.y;	
-        },
+	onTouchMove: function (event) {
+
+    	//make touch Event look like mouse event.
+		var rect = canvas.getBoundingClientRect();
+		InputEngine.actions['mousemove'] = true;
+		InputEngine.mouse.x = (event.targetTouches[0].clientX-rect.left+InputEngine.portPos.x) * Scale.x;
+       	InputEngine.mouse.y = (event.targetTouches[0].clientY-rect.top+InputEngine.portPos.y) * Scale.y;	
+		event.preventDefault();
+
+	},
+       
+    onMouseMove: function (event) {
+       	var rect = canvas.getBoundingClientRect();
+		InputEngine.actions['mousemove'] = true;
+		InputEngine.mouse.x = ((event.clientX+InputEngine.portPos.x)-rect.left) * Scale.x;
+       	InputEngine.mouse.y = ((event.clientY+InputEngine.portPos.y)-rect.top) * Scale.y;	
+	},
 
 
-        //-----------------------------
-        onKeyDown: function (event) {
-				
-                var action = InputEngine.bindings[event.keyCode];
-				console.log( event.keyCode );
-                if (action) {
-                        InputEngine.actions[action] = true;
+      
+    onKeyDown: function (event) {
+		
+		event.preventDefault();		
+    	
+		var action = InputEngine.bindings[event.keyCode];
+        	if (action) {
+            	InputEngine.actions[action] = true;
 						
-                }
+    		}
+	},
+
+   
+    onKeyUp: function (event) {
+		
+		event.preventDefault();	
+    	
+		var action = InputEngine.bindings[event.keyCode];
+
+        if (action) {
+        	InputEngine.actions[action] = false;
+		
+        }
         },
 
-   //-----------------------------
-        onKeyUp: function (event) {
-
-                var action = InputEngine.bindings[event.keyCode];
-
-                if (action) {
-                        InputEngine.actions[action] = false;
-						
-                }
-        },
-
-        //-----------------------------
+        
         bind: function (key, action) {
                 InputEngine.bindings[key] = action;
 				
--- scripts/core/SoundManager.js
+++ scripts/core/SoundManager.js
@@ -1,77 +1,159 @@
 SoundManager = {
 	//General variables
-	effectsVolume: 0.15,
-	
-	musicVolume: 1,
-	
 	offsetVolume: 0.1,
 	
 	globalMute: false,
 	
 	audioType: 'ogg',
 	
-	//Define the type of audio supported.
-	init: function(){
+	//Sound Variables
+	effectsVolume: 0.15,
 	
+	sounds: [],
+	
+	soundsLoaded: [],
+	
+	context: null,
+	
+	volumeNode: null,
+	
+	soundBuffers: {},
+	
+	soundArrays: {},
+	
+	counters: {},
+	
+	//Init Function	
+	init: function(array1, array2){
+	
+		//Define the type of audio supported.
 		var audio =  new Audio();
 		
 		if (audio.canPlayType('audio/ogg; codecs="vorbis"'))
 			this.audioType = 'ogg';
 		else
 			this.audioType = 'mp3';
-	},
+		
+		//Create AudioContext
+		if (typeof AudioContext !== "undefined")
+			this.context = new AudioContext();
+		else if (typeof webkitAudioContext !== "undefined")
+			this.context = new webkitAudioContext();
+		else
+			console.log('AudioContext not supported. :(');	
 	
-	numeberSpaces: 5, //DRAFT  ->> bullets per second * audio duration 
-	
-	main: new Array(this.numeberSpaces),
-	secondary: new Array(this.numeberSpaces),
-	c1: 0,
-	c2: 0,
-	 
-	loadGuns: function(src1, src2){
-		for(var j = 0; j < this.numeberSpaces; j++){
-			this.main[j] = Loader.load(src1 + '.' + this.audioType);
-			this.main[j].autoplay = false;
-			this.main[j].muted = this.globalMute;
-			this.main[j].volume = this.effectsVolume;	
-			this.secondary[j] = Loader.load(src1 + '.' + this.audioType);
-			this.secondary[j].autoplay = false;
-			this.secondary[j].muted = this.globalMute;
-			this.secondary[j].volume = this.effectsVolume;
+		//If AudioContext is not supported
+		if(this.context != null){
+		
+		//Create a volumeNode of AudioContext
+		this.volumeNode = this.context.createGainNode();
+		
+		this.volumeNode.gain.value = this.effectsVolume;
+		
+		this.volumeNode.connect(this.context.destination);
+		
+		
 		}
-	},
-	
-	playGun: function(i){
-		if( i === 1){
-			this.main[this.c1].play();
-			this.c1 = (this.c1 + 1) % this.main.length;
-		}
-		else{
-			this.secondary[this.c2].play();
-			this.c2 = (this.c2 + 1) % this.secondary.length;
-		}
+		
+		//Preload sounds - DRAFT
+		for(var i = 0; i < array1.length; i++)
+			this._loadSound(array1[i],array2[i]);
+		
+		
 		
 	},
 	
-	//Play the sound effect
+	//Play sound, if loaded - DRAFT
 	playSound: function(src){
-		try{
-			var audio = Loader.load(src + '.' + this.audioType);
+		//DRAFT
+		if(this.context != null){// return;
+		
+			var res = this._searchSound(src);
+			//DRAFT
+			if(res == -1){
+				console.log('Not Loaded Yet');
+				return;
+			}
+			
+			var source = this.context.createBufferSource();
+			source.buffer = this.soundBuffers[this.sounds[res]];
+			source.loop = false;
+			source.connect(this.volumeNode);
+			source.noteOn(0);
+		}
+		else{
+			var res = this._searchSound(src);
+			//DRAFT
+			if(res == -1){
+				console.log('Not Loaded Yet');
+				return;
+			}
+			var counter = this.counters[src];
+			var audio = this.soundArrays[src][counter];
 			audio.autoplay = false;
 			audio.muted = this.globalMute;
 			audio.volume = this.effectsVolume;
 			audio.play();
-		}
-		catch(e){
-			console.log('Error In Sound Play');
+			this.counters[src] = (this.counters[src] + 1) % this.soundArrays[src].length;
 		}
 	},
 	
-	musicArray: new Array(),
+	//Return the index of src in sounds if loaded
+	_searchSound: function(src){
+		return this.sounds.indexOf(src);
+	},
 	
+	
+	//Load the sounds - DRAFT
+	_loadSound: function(src, sps){ //sps = Sounds per second
+		if(this.context != null){
+			var request = new XMLHttpRequest();
+			
+			request.open('GET', src + '.' + this.audioType, true);
+			request.responseType = 'arraybuffer';
+			request.addEventListener('load',function(event){
+			
+				var request = event.target;
+				var buffer = SoundManager.context.createBuffer(request.response, false);
+				SoundManager.soundBuffers[src] = buffer;
+				SoundManager.sounds.push(src);
+				SoundManager.soundsLoaded.push(true);
+				
+			}, false);
+			
+			request.send();
+		}
+		else{
+			var audio = Loader.load(src + '.' + SoundManager.audioType);
+			var length = Math.ceil(sps * this.duration);
+			//DRAFT
+			length = 10;
+			var array = new Array(length);
+			for(var i = 0; i < length; i++){
+				array[i] = Loader.load(src + '.' + SoundManager.audioType);
+				array[i].autoplay = false;
+				array[i].muted = SoundManager.globalMute;
+				array[i].volume = SoundManager.effectsVolume;
+			}
+			SoundManager.soundArrays[src] = array;
+			SoundManager.counters[src] = 0;
+			SoundManager.sounds.push(src);
+		}
+	},
+	
+	//Music Variables
+	musicArray: new Array(),
+		
+	musicVolume: 1,
+		
 	maudio: new Audio(),
 	
+	mTempAudio: new Audio(),
+	
 	counter: 0,
+	
+	fadeTime: 5,
+	
 	//Load the playlist
 	loadMusic: function(array){
 		this.musicArray = array;
@@ -79,64 +161,122 @@ SoundManager = {
 		this.maudio.autoplay = false;
 		this.maudio.muted = this.globalMute;
 		this.maudio.volume = this.musicVolume;
+		this.maudio.next = false;
+		this.maudio.end = this.maudio.duration;
 		this.maudio.addEventListener("ended",function(){
-			SoundManager.nextTrack();
+			SoundManager.maudio = SoundManager.mTempAudio;
+			SoundManager.mTempAudio = null;
+		});
+		this.maudio.addEventListener("timeupdate",function(){
+			var d = this.end - this.currentTime;
+			var c = this.currentTime;
+			if(d <= SoundManager.fadeTime){
+				if(d < 0){
+					this.currentTime = this.duration;
+				}
+				else{
+					this.volume = (d / SoundManager.fadeTime) * SoundManager.musicVolume;
+					if(this.next === false){
+						this.next = true;
+						SoundManager.nextTrack();
+					}
+				}
+			}
+			if(c <= SoundManager.fadeTime)
+				this.volume = (c / SoundManager.fadeTime) * SoundManager.musicVolume;	
+			if(c > SoundManager.fadeTime && d > SoundManager.fadeTime)
+				this.volume = SoundManager.musicVolume;
 		});
 	},
+	
 	//Next track please!
 	nextTrack: function(){
+		if(!this.maudio.next){
+			this.maudio.next = true;
+			this.maudio.end = this.maudio.currentTime + SoundManager.fadeTime;
+		}
 		this.counter = (this.counter + 1) % this.musicArray.length;
-		this.maudio = Loader.load(this.musicArray[this.counter] + '.' + this.audioType);
-		this.maudio.autoplay = false;
-		this.maudio.muted = this.globalMute;
-		this.maudio.volume = this.musicVolume;
-		this.maudio.addEventListener("ended",function(){
-			SoundManager.nextTrack();
+		this.mTempAudio = Loader.load(this.musicArray[this.counter] + '.' + this.audioType);
+		this.mTempAudio.autoplay = false;
+		this.mTempAudio.muted = this.globalMute;
+		this.mTempAudio.volume = this.musicVolume;
+		this.mTempAudio.next = false;
+		this.mTempAudio.end = this.mTempAudio.duration;
+		this.mTempAudio.addEventListener("timeupdate",function(){
+			var d = this.end - this.currentTime;
+			var c = this.currentTime;
+			if(d <= SoundManager.fadeTime){
+				if(d < 0){
+					this.currentTime = this.duration;
+				}
+				else{
+					this.volume = (d / SoundManager.fadeTime) * SoundManager.musicVolume;
+					if(this.next === false){
+						this.next = true;
+						SoundManager.nextTrack();
+					}
+				}
+			}
+			if(c <= SoundManager.fadeTime)
+				this.volume = (c / SoundManager.fadeTime) * SoundManager.musicVolume;	
+			if(c > SoundManager.fadeTime && d > SoundManager.fadeTime)
+				this.volume = SoundManager.musicVolume;
 		});
-		this.playMusic()
+		this.mTempAudio.addEventListener("ended",function(){
+			SoundManager.maudio = SoundManager.mTempAudio;
+			SoundManager.mTempAudio = null;
+		});
+		this.mTempAudio.play();
 	},
+	
 	//Play the music
 	playMusic: function(){
 		this.maudio.play();	
 	},
+	
 	//Volume up the effects
 	volumeUpEffects: function(){
-		if(this.effectsVolume + offsetVolume <= 1)
-			this.effectsVolume += offsetVolume;
+		if(this.effectsVolume + this.offsetVolume <= 1)
+			this.volumeNode.gain.value += this.offsetVolume ;
 		else
-			this.effectsVolume = 1;	
+			this.volumeNode.gain.value = 1;	
 	},
+	
 	//Volume down the effects
 	volumeDownEffects: function(){
-		if(this.effectsVolume - offsetVolume >=0)
-			this.effectsVolume += offsetVolume;
+		if(this.effectsVolume - this.offsetVolume >=0)
+			this.volumeNode.gain.value -= this.offsetVolume;
 		else
-			this.effectsVolume = 0;
+			this.volumeNode.gain.value = 0;
 	},
+	
 	//Volume up the music
 	volumeUpMusic: function(){
-		if(this.musicVolume + musicVolume <= 1)
-			this.musicVolume += musicVolume;
+		if(this.musicVolume + this.offsetVolume <= 1)
+			this.musicVolume += this.offsetVolume;
 		else
 			this.musicVolume = 1;
 		this.maudio.volume = this.musicVolume;	
 	},
+	
 	//Volume down the music
 	volumeDownMusic: function(){
-		if(this.musicVolume - musicVolume >=0)
-			this.musicVolume += musicVolume;
+		if(this.musicVolume - this.offsetVolume >=0)
+			this.musicVolume -= this.offsetVolume;
 		else
 			this.musicVolume = 0;
 		this.maudio.volume = this.musicVolume;	
 	},
+	
 	//Mute the sound
 	muteAll: function(){
-		this.globalMute = true;
+		this.volumeNode.gain.value = 0;
 		this.maudio.muted = true;
 	},
+	
 	//Unmute the sound
 	umuteAll: function(){
-		this.globalMute = false;
+		this.volumeNode.gain.value = this.effectsVolume;
 		this.maudio.muted = false;
 	},
 	
@@ -147,19 +287,23 @@ SoundManager = {
 		this.volumeUpMusic();
 		this.volumeUpEffects();
 	},
+	
 	//Global volume down
 	globalVolumeUp: function(){
 		this.volumeUpMusic();
 		this.volumeUpEffects();
 	},
+	
 	//Effects volume
 	volumeEffects: function(){
 		return Math.floor(this.effectsVolume * 100);
 	},
+	
 	//Music volume
 	volumeMusic: function(){
 		return Math.floor(this.volumeMusic * 100);
 	},
+	
 	//Global volume
 	globalVolume: function(){
 		var diff = Math.abs(this.effectsVolume - this.volumeMusic);
@@ -170,14 +314,13 @@ SoundManager = {
 		else
 			return  Math.floor((this.effectsVolume + this.volumeMusic) * 100 / 2);
 	},
+	
 	//Supported audio type
 	soundType: function(){
 		return this.audioType;
-	}
-	
-	
+	}	
 }
-
-SoundManager.init();
-//Dratf Part
-SoundManager.loadGuns('sounds/LaserBeam0','sounds/LaserBeam1');
+//Draft
+SoundManager.init(['sounds/LaserBeam0', 'sounds/LaserBeam1', 'sounds/Explosion0'], [10, 10, 10]);
+SoundManager.loadMusic(['sounds/Fifty_Percent_-_Bright_Air', 'sounds/Air_Attack']);
+SoundManager.playMusic();
