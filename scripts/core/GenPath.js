3e2606dbbc705dd3cfd34f92f8bf4de6b2a0c8fe
lvrach <leo.al.vra@gmail.com>
2013-04-27 21:03:06 +0300

    GenPath improvments

--- scripts/core/GenPath.js
+++ scripts/core/GenPath.js
@@ -1,5 +1,6 @@
 GenPath = ({
 
+	maxNumPaths: 5,
 	paths: [],
 	map: [[]],
 	next: [],
@@ -7,25 +8,33 @@ GenPath = ({
 	DGD: 1,
 
 	init: function() {
-		this.map = [[0,0,0,0]]
-		this.paths = [0,1,2,3]
+		this.map = [[0,0,0,0,0]];
+		this.paths = [0,1,2,3,4];
+		this.maxNumPaths = this.paths.length;
 	},
 
 	step: function() {
 		var newPath;
 		var last = this.map[this.map.length - 1];
-		
+		var gc = [];
 		this.next = new Array(last.length);
 		
 		for (var i=0; i < this.next.length; i++) {
 			this.next[i] = null;
 		}
+
 		for (var i=0; i < this.paths.length; i++) {
 			
+			console.log(this.paths[i]);
+
+			if (this.paths[i] == null) {
+				continue;
+			}
+
 			var move = this.__computePath(this.paths[i]); 
 			
 			if ( move === false) {
-				this.paths.splice(i,1);
+				this.paths[i] = null;
 			}
 			else {
 				this.paths[i] == move;
@@ -35,12 +44,20 @@ GenPath = ({
 			
 		for (var i=0; i < this.next.length; i++) {
 			
-			if (this.next[i] == null) {
-				this.next[i] = 1;
+			if (last[i] == null ) {
+				last[i] = 1;
 			}
 		}
-		this.map.push(this.next);
 		
+		this.map.push(this.next);
+
+		//gc time
+		for (var i=0; i < this.paths.length; i++) {
+			if (this.paths[i] === null ) {
+				this.paths.slice(i,1);
+			}
+		}
+
 	},
 
 	__computePath: function (path) {
@@ -54,14 +71,13 @@ GenPath = ({
 		var posCost = [];
 		for (var p=0, i=0; i < last.length; i++) {
 			
-			if ( last[path] === null || 
+			if (last[path] === null || 
 				this._canTransition(last[path],last[i])) {
 				possiblePos[p] = i;		
-				posCost[p] = Math.pow(Math.E, Math.abs(i-path)) + 1;
+				posCost[p] = Math.pow(Math.E, -Math.abs(i-path));
 				p++;
 			}
 		}
-		console.log(posCost);	
 		//Normalize postCost
 		var sum = 0;
 		for (var i=0; i < posCost.length; i++) {
@@ -71,6 +87,7 @@ GenPath = ({
 			posCost[i] = posCost[i] / sum;
 		}
 
+		console.log(posCost);	
 		
 		//Move to random
 		var rand = Math.random();
@@ -82,15 +99,40 @@ GenPath = ({
 				break;
 			}
 		}
-		console.log("Move to:" + moveTo);
+		//forks
+		for (var i=0; i < this.maxNumPaths; i++) {
+			rand = Math.random();
+			if (rand < (this.paths.length-this.maxNumPaths)/this.maxNumPaths - 0.3) {
+				console.log('fork');
+				this.paths.push(path);
+			}
+		}
+		console.log(path + " Move to:" + moveTo);
 		
+			
+		if (path < moveTo) {
+			for (var i=path; i < moveTo; i++) {
+				last[i] = last[path]; //create vertical path
+				this.next[i] = 1; //set wall 
+			}			
+		}
+		else if (path > moveTo) {
+			for (var i=path; i > moveTo; i--) {
+				last[i] = last[path]; //create vertical path
+		//		if ( last[i] === null ) {
+					this.next[i] = 1;
+		//		}
+			}				
+		}
+
 		//Can I merge?
-		if ( moveTo != path && this.paths.indexOf() > 0 ) {
+		if (moveTo != path && this.paths.indexOf(path) > 0 ) {
 			//yes
 			return false; //remove path
-			console.log("merge");
+			console.log('Merge');
 		}
 		else {
+
 			//TODO
 			this.next[moveTo] = last[path];
 			return moveTo;
