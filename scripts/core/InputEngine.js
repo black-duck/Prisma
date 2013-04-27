518b0f65906eec8768d7f9ddaae3c460ab25121a
Giorgos Anagnostopoulos <ganag16@gmail.com>
2013-04-27 13:05:04 +0300

    Input Engine
    Fixed for prisma

--- scripts/core/InputEngine.js
+++ scripts/core/InputEngine.js
@@ -27,59 +27,22 @@ InputEngine = {
 		
 				
             //move key bindings
-            InputEngine.bind(87, 'look-up');
-            InputEngine.bind(65, 'look-left');
-            InputEngine.bind(68, 'look-right');
+            InputEngine.bind(87, 'go-up');
+            InputEngine.bind(83, 'go-down');
+			
+			InputEngine.bind(38, 'go-up');
+			InputEngine.bind(40, 'go-down');
 
-            //fire key bindings
-            InputEngine.bind(81,'powerup-use');  //Q
-            InputEngine.bind(69,'fire-superWeapon');  //E
-            InputEngine.bind(32,'fire-primary');  //Space
-            InputEngine.bind(16,'fire-missile');  //Shift   Ascii code=0 key code=16
-
+           
 			
             //event listeners
-        	canvas.addEventListener('mousedown',InputEngine.onMouseDown,true);
-			canvas.addEventListener('mouseout',InputEngine.onMouseUp,true);
-        	canvas.addEventListener('mouseup',InputEngine.onMouseUp,true);
             canvas.addEventListener('mousemove', InputEngine.onMouseMove,true);
             document.addEventListener('keydown', InputEngine.onKeyDown,true);
             document.addEventListener('keyup', InputEngine.onKeyUp,true);
-        	canvas.addEventListener('touchstart',InputEngine.onMouseDown,false);
-        	canvas.addEventListener('touchend',InputEngine.onMouseUp,false);
-			canvas.addEventListener('touchmove',InputEngine.onTouchMove,false);
+
         },
 
-	onMouseDown:function(event){
- 		var action = InputEngine.bindings[32];
-		if (event.button==2){
-			InputEngine.actions[action] = false;
-			return false;
-		}
- 		var rect = canvas.getBoundingClientRect();
-			InputEngine.mouse.x = (event.clientX-rect.left+InputEngine.portPos.x)*Scale.x;
-        	InputEngine.mouse.y = (event.clientY-rect.top+InputEngine.portPos.y)*Scale.y;
-		
-		if (action) {
-        	InputEngine.actions[action] = true;
-        }
-	},
-	onMouseUp:function(event){
-		var action = InputEngine.bindings[32];
-                if (action) {
-                      InputEngine.actions[action] = false;
-                }
-	},
-	onTouchMove: function (event) {
-
-    	//make touch Event look like mouse event.
-		var rect = canvas.getBoundingClientRect();
-		InputEngine.actions['mousemove'] = true;
-		InputEngine.mouse.x = (event.targetTouches[0].clientX-rect.left+InputEngine.portPos.x) * Scale.x;
-       	InputEngine.mouse.y = (event.targetTouches[0].clientY-rect.top+InputEngine.portPos.y) * Scale.y;	
-		event.preventDefault();
-
-	},
+	
        
     onMouseMove: function (event) {
        	var rect = canvas.getBoundingClientRect();
